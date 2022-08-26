b6ae9dabc4573e409fdcad9bf4ba27536ba30fdc
apmcguan <>
2022-08-26 15:37:53 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!\n";
+  cout<<"2+5= " << 2 + 5 << endl;
+  cout << "2/5=" << 2/5 << endl;
+  cout << "2/5.0=" << 2/5.0<< endl;
+  cout <<"10 % 4="<< 10%4 << endl; // % gives you the remainder  
+  cout <<" -10 % 4=" << -10%4<< endl;
+  cout << "2.35E-5 * 5.35E-3="<< 2.35E-5 * 5.35E-3<< endl;
+  double a = 3.5;
+  int b=2;
+  int c = 1;
+  int d;
+  d= a / b + c;
+  double e;
+  e= a / b +c;
+  cout<< "a/b+c="<<a / b + c<<endl;
+  cout<<"d="<<d<<endl;
+  cout<<"e="<< a / b + c<< endl;
+ 
+  }
